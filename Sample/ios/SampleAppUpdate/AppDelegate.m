f996b26823b0fb8b0949c72857b0b1f4e97718d0
Olivier Demolliens <Olivier.Demolliens@arhs-spikeseed.com>
2018-06-03 15:15:51 +0200

    Add sample + link

---
+++ Sample/ios/SampleAppUpdate/AppDelegate.m
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <ODAppUpdate/AppUpdateListenerSingleton.h>
+#import <ODAppUpdate/AppVersionListener.h>
+
+@interface AppDelegate()<AppVersionListener>
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  
+  [[AppUpdateListenerSingleton sharedManager]setAppListener:self];
+  
+  NSURL *jsCodeLocation;
+
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"SampleAppUpdate"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  return YES;
+}
+
+-(void)checkMigrationAppVersion:(NSString*)previousversion andCurrentVersion:(NSString*)currentversion{
+  if([previousversion isEqualToString:@"1.0.1"] && [currentversion isEqualToString:@"1.0.2"]){
+    
+  }
+}
+
+@end
