81560087e45c48a97518bce256d88e7a4178c9c0
odemolliens <olivier.demolliens@gmail.com>
2018-12-13 14:55:15 +0100

    Remove sample and put a new one

--- SampleAppUpdate/App.js
+++ SampleAppUpdate/App.js
@@ -1,55 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import { NativeModules } from 'react-native';
-const { ODAppUpdate } = NativeModules;
-
-
-
-const instructions = Platform.select({
-  ios: 'Hello world',
-  android: 'Hello world',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-
-  constructor(){
-    super();
-    ODAppUpdate.appVersion()
-      .then(response => {
-        // New version installed
-        console.log(response)
-      })
-        .catch(error => {
-            // No update installed (or error occured)
-          console.warn(error.message)
-        });
-  }
-
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
@@ -58,18 +14,8 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    justifyContent: 'center',
   },
 });
--- SampleAppUpdate/ios/SampleAppUpdate/AppDelegate.m
+++ SampleAppUpdate/ios/SampleAppUpdate/AppDelegate.m
@@ -22,17 +22,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [AppUpdateListenerSingleton sharedManager:self];
-
+  
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"SampleAppUpdate"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
